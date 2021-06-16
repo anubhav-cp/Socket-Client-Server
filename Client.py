96125be8b675ae43b8e18802c809329ebe3d862d
Anubhav <anubhavtiwaricp@gmail.com>
2021-06-16 22:45:20 +0530

    Multi Users Support

--- Client.py
+++ Client.py
@@ -1,21 +1,43 @@
 import socket
+import threading
 
-HEADER = 64
 PORT = 5050
-FORMAT = 'utf-8'
-DISCONNECT_MSG = "!DISCONNECT_MSG"
+FORMAT = 'ascii'
 SERVER = socket.gethostbyname(socket.gethostname())
 
-
+nickname = input("Enter Username: ")
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((SERVER, PORT))
 
-def send(msg):
-    message = msg.encode(FORMAT)
-    msg_length = len(message)
-    send_length = str(msg_length).encode(FORMAT)
-    send_length += b' ' * (HEADER - len(send_length))
-    client.send(send_length)
-    client.send(message)
+def write():
+    run = True
+    while run:
+        msg = f"{nickname}: {input('')}"
+        if msg == "quit":
+            break
+        message = msg.encode(FORMAT)
+        client.send(message)
 
-send("Hello World")
+
+def receive():
+    while True:
+        try:
+            message = client.recv(1024).decode(FORMAT)
+            if message == 'WEB':
+                client.send(nickname.encode(FORMAT))
+            else:
+                print(message)
+
+        except:
+            print("An Error Occured")
+            client.close()
+            break
+
+receive_thread = threading.Thread(target = receive)
+receive_thread.start()
+
+write_thread = threading.Thread(target = write)
+write_thread.start()
+
+
+
--- Server.py
+++ Server.py
@@ -1,40 +1,57 @@
 import socket
 import threading
 
-HEADER = 64
 PORT = 5050
 SERVER = socket.gethostbyname(socket.gethostname())
-FORMAT = 'utf-8'
-DISCONNECT_MSG = "!DISCONNECT_MSG"
+FORMAT = 'ascii'
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((SERVER, PORT))
-
-def handle_client(conn, addr):
-    print(f"[New Connection] {addr} connected")
-
-    connected = True
-    while connected:
-        msg_length = conn.recv(HEADER).decode(FORMAT)
-        if msg_length:
-            msg_length = int(msg_length)
-            msg = conn.recv(msg_length).decode(FORMAT)
-            if msg == DISCONNECT_MSG:
-                connected = False
-
-            print(f"[{addr}] {msg}")
-    conn.close()
+server.listen()
 
 
-def start():
-    server.listen()
+clients = []
+nicknames = []
+
+
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+
+def handle_client(client):
     while True:
-        conn, addr = server.accept()
-        thread = threading.Thread(target = handle_client, args=(conn, addr))
+        try:
+            message = client.recv(1024)
+            broadcast(message)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            nicknames.remove(nickname)
+            break
+
+
+def receive():
+
+    while True:
+        client, addr = server.accept()
+        print(f"Connected with {str(addr)}")
+
+        client.send('WEB'.encode(FORMAT))
+        nickname = client.recv(1024).decode(FORMAT)
+        nicknames.append(nickname)
+        clients.append(client)
+        print(f"Nickname of the client is {nickname}!")
+        broadcast(f"{nickname} joined the Chat!".encode(FORMAT))
+        client.send("Connected to the Server!".encode(FORMAT))
+
+        thread = threading.Thread(target = handle_client, args = (client,))
         thread.start()
-        print(f"[Active Connections] {threading.activeCount() - 1}")
+
 
 
 print("[STARTING] Starting the Server...")
-start()
+receive()
 
